26a3e9db4393a7c8e732e429f388b3ee58ba0a02
shule517 <shule517@gmail.com>
2013-03-10 11:13:13 +0900

    ・Observerクラスの追加 - Observer：観測者 - Observal：観測対象

---
+++ src/Shule.Peerst/Observer/Observable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shule.Peerst.Observer
+{
+	/// <summary>
+	/// オブザーバの通知先：観測対象
+	/// </summary>
+	public class Observable
+	{
+		/// <summary>
+		/// オブザーバリスト
+		/// </summary>
+		List<Observer> observers = new List<Observer>();
+
+		/// <summary>
+		/// オブザーバの追加
+		/// </summary>
+		/// <param name="observer">追加対象のオブザーバ</param>
+		void AddObserver(Observer observer)
+		{
+			observers.Add(observer);
+		}
+
+		/// <summary>
+		/// オブザーバの削除
+		/// </summary>
+		/// <param name="observer">削除対象のオブザーバ</param>
+		void DeleteObserver(Observer observer)
+		{
+			observers.Remove(observer);
+		}
+
+		/// <summary>
+		/// オブザーバへ通知
+		/// </summary>
+		/// <param name="param">渡すデータ</param>
+		void NotifyObservers(Object param)
+		{
+			foreach(Observer observer in observers)
+			{
+				observer.OnUpdate(param);
+			}
+		}
+
+		/// <summary>
+		/// オブザーバへ通知
+		/// </summary>
+		void NotifyObservers()
+		{
+			NotifyObservers(new Object());
+		}
+	}
+}
