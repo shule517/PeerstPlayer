6e9712cdd4cd888624012d58a69dbe6637d66014
shule517 <shule517@gmail.com>
2013-05-01 00:08:24 +0900

    追加：動画プレイヤー設定情報クラス

---
+++ PeerstPlayer/PeerstPlayer/Model/PlayerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerstPlayer.Model
+{
+	// 動画プレイヤーの設定情報
+	static class PlayerSettings
+	{
+	}
+}
