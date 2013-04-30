5ad3df472ddb570477a3f5171029674f20ab5a82
shule517 <shule517@gmail.com>
2013-05-01 00:00:21 +0900

    修正：フォームの移動

--- PeerstPlayer/PeerstPlayer/View/PlayerView.cs
+++ PeerstPlayer/PeerstPlayer/View/PlayerView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PeerstPlayer
 {
-	public partial class Form1 : Form
+	public partial class PlayerView : Form
 	{
-		public Form1()
+		public PlayerView()
 		{
 			InitializeComponent();
 		}
--- PeerstPlayer/PeerstViewer/View/ViewerView.cs
+++ PeerstPlayer/PeerstViewer/View/ViewerView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PeerstViewer
 {
-	public partial class Form1 : Form
+	public partial class ViewerView : Form
 	{
-		public Form1()
+		public ViewerView()
 		{
 			InitializeComponent();
 		}
