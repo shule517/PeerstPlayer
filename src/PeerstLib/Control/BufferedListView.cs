6ce62da554ee6a74e88abf0c86f324e61df5050b
shule517 <shule517@gmail.com>
2013-05-08 22:35:59 +0900

    コントロールの追加 ・ToolStripの継承 ・ListViewの継承

---
+++ src/PeerstLib/Control/BufferedListView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PeerstLib.Control
+{
+	// ListViewクラス
+	// チラつきを軽減
+	public class BufferedListView : ListView
+	{
+		protected override bool DoubleBuffered { get { return true; } set { } }
+	}
+}
---
+++ src/PeerstLib/Control/ToolStripEx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PeerstLib.Control
+{
+	// ToolStripクラス
+	// フォーカスが当たっていない状態でのボタン押下に対応
+	public class ToolStripEx : ToolStrip
+	{
+		public ToolStripEx()
+		{
+		}
+
+		const uint WM_MOUSEACTIVATE = 0x21;
+		const uint MA_ACTIVATE = 1;
+		const uint MA_ACTIVATEANDEAT = 2;
+
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+
+			if ((m.Msg == WM_MOUSEACTIVATE) && (m.Result == (IntPtr)MA_ACTIVATEANDEAT))
+			{
+				m.Result = (IntPtr)MA_ACTIVATE;
+			}
+		}
+	}
+}
