0eb1aa46f5afcdc0a01446c8147f5502ea9c81de
shule517 <shule517@gmail.com>
2013-05-08 22:57:30 +0900

    PecaPlayerコントロール ・プロパティの追加

--- src/PeerstPlayer/Control/PecaPlayer.cs
+++ src/PeerstPlayer/Control/PecaPlayer.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using PeerstLib.PeerCast;
+using WMPLib;
 
 namespace PeerstPlayer.Control
 {
@@ -15,6 +16,35 @@ namespace PeerstPlayer.Control
 	{
 		// チャンネル情報
 		public ChannelInfo ChannelInfo { get; set; }
+
+		// ミュート
+		public bool Mute { get { return wmp.settings.mute; } set { wmp.settings.mute = value; } }
+
+		// 音量
+		public int Volume { get { return wmp.settings.volume; } set { wmp.settings.volume = value; } }
+
+		// 再生時間
+		public string Duration
+		{
+			get
+			{
+				string position = wmp.Ctlcontrols.currentPositionString;
+				if (position.Length == 5)
+				{
+					position = "00:" + position;
+				}
+
+				return position;
+			}
+		}
+
+		// 再生状態
+		public WMPPlayState PlayState
+		{
+			get { return wmp.playState; }
+		}
+
+		// チャンネル情報更新イベント
 		public event EventHandler ChannelInfoChange;
 
 		// マウス押下イベント
@@ -31,6 +61,8 @@ namespace PeerstPlayer.Control
 			// 初期設定
 			wmp.uiMode = "none";
 			wmp.stretchToFit = true;
+
+			// マウス押下イベント
 			wmp.MouseDownEvent += (sender, e) =>
 			{
 				if (MouseDownEvent != null) MouseDownEvent(sender, new EventArgs());
@@ -60,17 +92,26 @@ namespace PeerstPlayer.Control
 
 			// チャンネル情報の取得
 			// TODO BackGroundで実行する
-			this.ChannelInfo = pecaConnect.GetChannelInfo();
-			if (ChannelInfoChange != null)
+			Timer timer = new Timer();
+			timer.Interval = UpdateInterval;
+			timer.Tick += (sender, e) =>
 			{
-				ChannelInfoChange(this, new EventArgs());
-			}
+				this.ChannelInfo = pecaConnect.GetChannelInfo();
+				if (ChannelInfoChange != null)
+				{
+					ChannelInfoChange(this, new EventArgs());
+				}
+			};
+			timer.Start();
 		}
 
 		#region 非公開プロパティ
 
 		// PeerCast通信
 		private PeerCastConnection pecaConnect = null;
+
+		// チャンネル更新間隔
+		private const int UpdateInterval = 10000;
 
 		#endregion
 	}
