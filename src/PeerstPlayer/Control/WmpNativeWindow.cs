7449a1fed8fe861e155b1f15d1f32e5c735bc462
shule517 <shule517@gmail.com>
2013-05-24 07:17:35 +0900

    追加：WMPウィンドウのサブクラス化

---
+++ src/PeerstPlayer/Control/WmpNativeWindow.cs
@@ -0,0 +1,39 @@
+﻿using PeerstLib.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PeerstPlayer.Control
+{
+	// WMPウィンドウのサブクラス化
+	class WmpNativeWindow : NativeWindow
+	{
+		// ダブルクリックイベント
+		public event EventHandler DoubleClick = delegate { };
+
+		// コンストラクタ
+		public WmpNativeWindow(IntPtr handle)
+		{
+			// サブクラスウィンドウの設定
+			AssignHandle(handle);
+		}
+
+		// ウィンドウプロシージャ
+		protected override void WndProc(ref Message m)
+		{
+			switch ((WindowMessage)m.Msg)
+			{
+				case WindowMessage.WM_LBUTTONUP:
+					DoubleClick(this, new EventArgs());
+					break;
+
+				default:
+					break;
+			}
+
+			base.WndProc(ref m);
+		}
+	}
+}
