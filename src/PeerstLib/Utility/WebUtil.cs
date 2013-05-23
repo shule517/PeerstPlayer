52a3ed1b873f77416abd3e0238bc899530d75366
shule517 <shule517@gmail.com>
2013-05-23 23:44:51 +0900

    WebUtiliクラスの追加 ・HTML取得メソッド

---
+++ src/PeerstLib/Utility/WebUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace PeerstLib.Utility
+{
+	static public class WebUtil
+	{
+		//-------------------------------------------------------------
+		// 概要：HTMLの取得
+		// 詳細：subject.txtからスレッド一覧情報を作成する
+		//-------------------------------------------------------------
+		public static string GetHtml(string url, Encoding encoding)
+		{
+			try
+			{
+				WebClient wc = new WebClient();
+				byte[] data = wc.DownloadData(url);
+				return encoding.GetString(data);
+			}
+			catch
+			{
+				return "";
+			}
+		}
+	}
+}
