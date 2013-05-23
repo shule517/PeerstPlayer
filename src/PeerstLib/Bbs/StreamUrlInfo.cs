d07d0876d788252a0fb5c53dd5e0fd93140ad4b6
shule517 <shule517@gmail.com>
2013-05-24 07:18:38 +0900

    追加：ストリームURLから抽出した情報クラス

---
+++ src/PeerstLib/Bbs/StreamUrlInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeerstLib.Bbs
+{
+	// ストリームURLから抽出した情報クラス
+	public class StreamUrlInfo
+	{
+		public StreamUrlInfo()
+		{
+			Host = "";
+			PortNo = "";
+			StreamId = "";
+		}
+
+		public string Host { get; set; }		// PeerCastのアドレス
+		public string PortNo { get; set; }		// PeerCastのポート番号
+		public string StreamId { get; set; }	// ストリームID
+	}
+}
