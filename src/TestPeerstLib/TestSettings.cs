c80613682d02dc763d99a2703f20d523f63874ea
shule517 <shule517@gmail.com>
2013-05-25 12:13:12 +0900

    テストの設定クラスを追加

--- src/TestPeerstLib/TestSettings.cs
+++ src/TestPeerstLib/TestSettings.cs
@@ -9,7 +9,7 @@ namespace TestPeerstLib
 	public static class TestSettings
 	{
 		// PeerCast動画
-		public static string StreamUrl = "http://localhost:7145/pls/90E13182A11873DF1B8ADD5F4E7C0A38?tip=183.181.158.208:7154";
+		public static string StreamUrl = "http://localhost:7145/pls/90E13182A11873DF1B8ADD5F4E7C0A38?tip=210.159.254.138:7154";
 
 		// ローカル動画(WMV)
 		public static string LocalMoviePath
