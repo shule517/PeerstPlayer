a97ad94250b66729df7abd8921762526e7c5b675
shule517 <shule517@gmail.com>
2013-09-15 09:21:29 +0900

    追加：ログ出力レベルの変更メソッド

--- src/PeerstLib/Util/Logger.cs
+++ src/PeerstLib/Util/Logger.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
 using log4net;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
 
 namespace PeerstLib.Util
 {
@@ -29,6 +31,15 @@ namespace PeerstLib.Util
 				MethodBase callerMethod = callerFrame.GetMethod();
 				return LogManager.GetLogger(callerMethod.DeclaringType);
 			}
+		}
+
+		//-------------------------------------------------------------
+		// 概要：ログ出力レベルの変更
+		//-------------------------------------------------------------
+		public static void ChangeLogLevel(Level level)
+		{
+			var rep = LogManager.GetRepository() as Hierarchy;
+			rep.Threshold = level;
 		}
 	}
 }
