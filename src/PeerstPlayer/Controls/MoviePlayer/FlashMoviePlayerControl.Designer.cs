aa4f02fe2db58aad023c9d434dcf275a6f09f18b
shule517 <shule517@gmail.com>
2013-12-01 06:50:11 +0900

    Flash動画プレイヤーコントロールの追加

---
+++ src/PeerstPlayer/Controls/MoviePlayer/FlashMoviePlayerControl.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PeerstPlayer.Controls.MoviePlayer
+{
+	partial class FlashMoviePlayerControl
+	{
+		/// <summary> 
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region コンポーネント デザイナーで生成されたコード
+
+		/// <summary> 
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlashMoviePlayerControl));
+			this.axShockwaveFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// axShockwaveFlash
+			// 
+			this.axShockwaveFlash.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.axShockwaveFlash.Enabled = true;
+			this.axShockwaveFlash.Location = new System.Drawing.Point(0, 0);
+			this.axShockwaveFlash.Name = "axShockwaveFlash";
+			this.axShockwaveFlash.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash.OcxState")));
+			this.axShockwaveFlash.Size = new System.Drawing.Size(150, 150);
+			this.axShockwaveFlash.TabIndex = 0;
+			// 
+			// FlashMoviePlayerControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.Controls.Add(this.axShockwaveFlash);
+			this.Name = "FlashMoviePlayerControl";
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash;
+	}
+}
---
+++ src/PeerstPlayer/Controls/MoviePlayer/FlashMoviePlayerControl.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace PeerstPlayer.Controls.MoviePlayer
+{
+	/// <summary>
+	/// Flash動画プレイヤーコントロール
+	/// </summary>
+	public partial class FlashMoviePlayerControl : UserControl, IMoviePlayer
+	{
+		public FlashMoviePlayerControl()
+		{
+			InitializeComponent();
+
+			MouseDown+= (e, sender) =>
+			{
+				mouseDownEvent(this, new AxWMPLib._WMPOCXEvents_MouseDownEvent(0 ,0, 0, 0));
+			};
+		}
+
+		int IMoviePlayer.Volume
+		{
+			get { return 0; }
+			set { }
+		}
+
+		int IMoviePlayer.VolumeBalance
+		{
+			get { return 0; }
+			set { }
+		}
+
+		event EventHandler IMoviePlayer.VolumeChange
+		{
+			add { }
+			remove { }
+		}
+
+		event EventHandler IMoviePlayer.MovieStart
+		{
+			add { }
+			remove { }
+		}
+
+		bool IMoviePlayer.Mute
+		{
+			get { return false; }
+			set { }
+		}
+
+		string IMoviePlayer.Duration
+		{
+			get { return String.Empty; }
+		}
+
+		int IMoviePlayer.BufferingProgress
+		{
+			get { return 0; }
+		}
+
+		WMPLib.WMPPlayState IMoviePlayer.PlayState
+		{
+			get { return WMPPlayState.wmppsUndefined; }
+		}
+
+		WMPLib.WMPOpenState IMoviePlayer.OpenState
+		{
+			get { return WMPOpenState.wmposUndefined; }
+		}
+
+		float IMoviePlayer.AspectRate
+		{
+			get { return 1.0f; }
+		}
+
+		int IMoviePlayer.ImageWidth
+		{
+			get { return 800; }
+		}
+
+		int IMoviePlayer.ImageHeight
+		{
+			get { return 600; }
+		}
+
+		event AxWMPLib._WMPOCXEvents_MouseDownEventHandler mouseDownEvent;
+		event AxWMPLib._WMPOCXEvents_MouseDownEventHandler IMoviePlayer.MouseDownEvent
+		{
+			add { mouseDownEvent += value; }
+			remove { mouseDownEvent -= value; }
+		}
+
+		event AxWMPLib._WMPOCXEvents_MouseUpEventHandler IMoviePlayer.MouseUpEvent
+		{
+			add { }
+			remove { }
+		}
+
+		event AxWMPLib._WMPOCXEvents_MouseMoveEventHandler IMoviePlayer.MouseMoveEvent
+		{
+			add { }
+			remove { }
+		}
+
+		event EventHandler IMoviePlayer.DoubleClickEvent
+		{
+			add { }
+			remove { }
+		}
+
+		event AxWMPLib._WMPOCXEvents_KeyDownEventHandler IMoviePlayer.KeyDownEvent
+		{
+			add { }
+			remove { }
+		}
+
+		IntPtr IMoviePlayer.WMPHandle
+		{
+			get { return IntPtr.Zero; }
+		}
+
+		bool IMoviePlayer.EnableContextMenu
+		{
+			get { return false; }
+			set { }
+		}
+
+		int IMoviePlayer.NowFrameRate
+		{
+			get { return 0; }
+		}
+
+		int IMoviePlayer.FrameRate
+		{
+			get { return 0; }
+		}
+
+		int IMoviePlayer.NowBitrate
+		{
+			get { return 0; }
+		}
+
+		int IMoviePlayer.Bitrate
+		{
+			get { return 0; }
+		}
+
+		Control IMoviePlayer.MovieControl
+		{
+			get { return new Control(); }
+		}
+
+		void IMoviePlayer.PlayMoive(string streamUrl)
+		{
+		}
+	}
+}
