2b1fac97562af2591258dcdb4f0498c9c1e6518a
shule517 <shule517@gmail.com>
2014-01-09 23:40:08 +0900

    log4netの更新

--- src/PeerstViewer/Program.cs
+++ src/PeerstViewer/Program.cs
@@ -1,20 +1,30 @@
-﻿using PeerstViewer.ThreadViewer;
+﻿using log4net;
+using PeerstLib.Util;
+using PeerstViewer.ThreadViewer;
 using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace PeerstViewer
 {
 	static class Program
 	{
+		// log4netの初期化
+		private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
 		/// <summary>
 		/// アプリケーションのメイン エントリ ポイントです。
 		/// </summary>
 		[STAThread]
 		static void Main()
 		{
+			Logger.Instance.Info("START:PeerstViewer");
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new ThreadViewerView());
+
+			Logger.Instance.Info("END:PeerstViewer");
 		}
 	}
 }
